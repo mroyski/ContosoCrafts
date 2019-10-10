9cecf43c0eb35f6cbc727c33b8448211396dc527
Michael <mroyski@gmail.com>
2019-10-10 14:28:38 -0400

    added json serializer

--- ContosoCrafts.WebSite/Models/Product.cs
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -17,5 +18,7 @@ namespace ContosoCrafts.WebSite.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
+        public override string ToString() =>
+            JsonSerializer.Serialize<Product>(this);
     }
 }
